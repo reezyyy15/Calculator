54e2efaf41e677725d8ae3eadf903104b4df688d
Reezy15 <leomakombe15@gmail.com>
2024-04-01 22:34:00 +0200

    First Commit

---
+++ src/OperationButton.js
@@ -0,0 +1,10 @@
+import {ACTION} from './App'
+
+export default function OperationButton({dispatch, operation}) {
+return ( 
+<button onClick={() => dispatch ({type: ACTION.CHOOSE_OPERATION, payload:{operation}})}
+>
+    {operation}
+    </button>
+)
+}
